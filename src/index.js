2e6e29e1f3ab38dbcc00b45d80a3c92904933f90
Navjeevan Alone <navjeevanalone@gmail.com>
2022-08-13 22:34:37 +0530

    minor

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-// import reportWebVitals from "./reportWebVitals";
+import reportWebVitals from "./reportWebVitals";
 import StateProvider from "./contexts/StateContext";
 import reducer, { initialState } from "./reducers/stateReducer";
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -14,4 +14,4 @@ root.render(
 	</React.StrictMode>
 );
 
-// reportWebVitals();
+reportWebVitals();
